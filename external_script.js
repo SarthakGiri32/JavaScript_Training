706ac84863fd597c49ce8b1a00c9a9f0fd3acf2e
Sarthak Giri <giri.sarthak1@gmail.com>
2024-02-01 22:57:43 +0530

    Adding code for JS training

---
+++ external_script.js
@@ -0,0 +1,3 @@
+function changeText(){
+	document.getElementById('change_text').innerHTML = 'Paragraph Changed!';
+}
---
+++ main.js
@@ -0,0 +1,8 @@
+// Using console.log 
+// console.log('Hello World');
+
+// document.write("Hello World!");
+
+function displayAlert(){
+    alert("Hello World!");
+}
